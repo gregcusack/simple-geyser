8ef4068abd5d81dd202297854766ea18e4a7cc42
greg <greg@anza.xyz>
2024-10-30 00:31:52 +0000

    add bandwidth but not working

--- src/plugin.rs
+++ src/plugin.rs
@@ -4,14 +4,57 @@ use {
         GeyserPlugin, ReplicaAccountInfoVersions, Result as PluginResult,
         ReplicaTransactionInfoVersions, FfiNode, FfiPubkey,
     },
-    solana_program::slot_history::Slot, 
     solana_sdk::pubkey::Pubkey,
+    solana_program::slot_history::Slot,
+    std::{
+        sync::{Mutex, atomic::{AtomicU64, Ordering}},
+        time::{Duration, Instant},
+    },
+    
 };
 
 pub const RUST_LOG_FILTER: &str = "info";
 
-#[derive(Debug, Default)]
-pub struct SimplePlugin {}
+#[derive(Debug)]
+pub struct SimplePlugin {
+    event_count: AtomicU64,
+    // byte_count: AtomicU64,
+    last_measurement: Mutex<Instant>,
+}
+
+impl Default for SimplePlugin {
+    fn default() -> Self {
+        SimplePlugin {
+            event_count: AtomicU64::new(0),
+            // byte_count: AtomicU64::new(0),
+            last_measurement: Mutex::new(Instant::now()),
+        }
+    }
+}
+
+impl SimplePlugin {
+    fn log_and_reset_metrics(&self) {
+        let mut last_measurement = self.last_measurement.lock().unwrap(); // Lock for exclusive access
+        let elapsed = last_measurement.elapsed();
+        
+        if elapsed >= Duration::from_secs(1) {
+            // Swap and reset the counters
+            let events = self.event_count.swap(0, Ordering::Relaxed);
+            // let bytes = self.byte_count.swap(0, Ordering::Relaxed);
+
+            let rate = events as f64 / elapsed.as_secs_f64();
+            // let bandwidth = bytes as f64 / elapsed.as_secs_f64();
+
+            info!(
+                "greg: event rate: {:.2} events/s",
+                rate
+            );
+
+            // Update `last_measurement` to the current time
+            *last_measurement = Instant::now();
+        }
+    }
+}
 
 impl GeyserPlugin for SimplePlugin {
     fn name(&self) -> &'static str {
@@ -52,7 +95,7 @@ impl GeyserPlugin for SimplePlugin {
     }
 
     fn account_data_notifications_enabled(&self) -> bool {
-        false // process account changes
+        true // process account changes
     }
 
     fn transaction_notifications_enabled(&self) -> bool {
@@ -77,6 +120,17 @@ impl GeyserPlugin for SimplePlugin {
 
     fn notify_node_update(&self, ffi_node: &FfiNode) -> PluginResult<()> {
         info!("greg: ffi_node -> pk: {}, wc: {}, sv: {}", Pubkey::from(ffi_node.pubkey.pubkey), ffi_node.wallclock, ffi_node.shred_version);
+
+        // let serialized_value = bincode::serialize(&ffi_node).expect("Failed to serialize value");
+        // let byte_len = serialized_value.len() as u64;
+
+        // Increment counters
+        self.event_count.fetch_add(1, Ordering::Relaxed);
+        // self.byte_count.fetch_add(byte_len, Ordering::Relaxed);
+        
+        // Check if we need to log and reset the rate/bandwidth metrics
+        self.log_and_reset_metrics();
+
         Ok(())
     }
 

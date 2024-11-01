d6e76c50e9cfae1ca3d241ee8b7f2582d05c960d
greg <greg@anza.xyz>
2024-11-01 21:57:08 +0000

    switch to node update and removal plugin

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,15 +1,14 @@
-use { 
+use {
+    log::*,
     agave_geyser_plugin_interface::geyser_plugin_interface::{
         GeyserPlugin, ReplicaAccountInfoVersions, Result as PluginResult,
-        ReplicaTransactionInfoVersions,
+        ReplicaTransactionInfoVersions, FfiNode, FfiPubkey,
     },
-    solana_program::{
-        slot_history::Slot,
-        pubkey::Pubkey,
-    },
-    solana_gossip::crds::VersionedCrdsValue,
-    
+    solana_program::slot_history::Slot, 
+    solana_sdk::pubkey::Pubkey,
 };
+
+pub const RUST_LOG_FILTER: &str = "info";
 
 #[derive(Debug, Default)]
 pub struct SimplePlugin {}
@@ -20,6 +19,7 @@ impl GeyserPlugin for SimplePlugin {
     }
 
     fn on_load(&mut self, _config_file: &str, _is_reload: bool) -> PluginResult<()> {
+        solana_logger::setup_with_default(RUST_LOG_FILTER); // Ensure logging is initialized
         Ok(())
     }
 
@@ -38,7 +38,7 @@ impl GeyserPlugin for SimplePlugin {
             ReplicaAccountInfoVersions::V0_0_3(account_info) => account_info.pubkey,
         };
 
-        println!(
+        info!(
             "account {:?} updated at slot {}!",
             Pubkey::try_from(pubkey_bytes).unwrap(),
             slot
@@ -66,21 +66,26 @@ impl GeyserPlugin for SimplePlugin {
     ) -> PluginResult<()> {
         match transaction {
             ReplicaTransactionInfoVersions::V0_0_1(transaction_info) => {
-                println!("transaction {:#?} at slot {}!", transaction_info, slot);
+                info!("transaction {:#?} at slot {}!", transaction_info, slot);
             }
             ReplicaTransactionInfoVersions::V0_0_2(transaction_info) => {
-                println!("transaction {:#?} at slot {}!", transaction_info, slot);
+                info!("transaction {:#?} at slot {}!", transaction_info, slot);
             }
         }
         Ok(())
     }
 
-    fn insert_crds_value(&self, value: VersionedCrdsValue) -> PluginResult<()> {
-        println!("greg: rx origin pk: {}", value.value.pubkey());
+    fn notify_node_update(&self, ffi_node: &FfiNode) -> PluginResult<()> {
+        info!("greg: ffi_node -> pk: {}, wc: {}, sv: {}", Pubkey::from(ffi_node.pubkey.pubkey), ffi_node.wallclock, ffi_node.shred_version);
         Ok(())
     }
 
-    fn gossip_messages_notifications_enabled(&self) -> bool {
+    fn notify_node_removal(&self, pubkey: &FfiPubkey) -> PluginResult<()> {
+        info!("greg: pk removed pk: {}", Pubkey::from(pubkey.pubkey));
+        Ok(())
+    }
+
+    fn node_update_notifications_enabled(&self) -> bool {
         true
     }
 
